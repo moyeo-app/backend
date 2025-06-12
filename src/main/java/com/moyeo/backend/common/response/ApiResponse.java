e3df0a75d37c96ea0c29eae831510cedfefb7f5e
jelliclesu <jelliclesu@naver.com>
2025-06-12 18:24:09 +0900

    Feat: [사용자 관리] 공통 응답 생성 (MOYEO-29)

---
+++ src/main/java/com/moyeo/backend/common/response/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.moyeo.backend.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiResponse<T> (
+        String status,
+        int code,
+        String message,
+        T data
+) {
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>("success", 200, "API 요청이 성공했습니다.", data);
+    }
+
+    public static ApiResponse<Object> fail(ResponseCode responseCode) {
+        return new ApiResponse<>("fail", responseCode.getStatus().value(), responseCode.getMessage(), null);
+    }
+}
