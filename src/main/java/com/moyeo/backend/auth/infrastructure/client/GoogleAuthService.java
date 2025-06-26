b67871385b6d32a2b78d7a6562bc7b313b7d3f4b
jelliclesu <jelliclesu@naver.com>
2025-06-26 17:37:35 +0900

    Fix: [사용자 관리] 구글 소셜 로그인 API  (MOYEO-28)
    - 토큰 발급 추가

--- src/main/java/com/moyeo/backend/auth/infrastructure/client/GoogleAuthService.java
+++ src/main/java/com/moyeo/backend/auth/infrastructure/client/GoogleAuthService.java
@@ -7,22 +7,68 @@ import com.moyeo.backend.auth.infrastructure.dtos.TokenResponse;
 import com.moyeo.backend.common.enums.ErrorCode;
 import com.moyeo.backend.common.exception.CustomException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j(topic = "GoogleAuthService")
 @Service("google")
 public class GoogleAuthService extends AbstractOAuthProviderService{
-    private static final String API_BASE_URL = "https://www.googleapis.com/oauth2/v3/userinfo";
+    private static final String API_BASE_URL_TOKEN = "https://oauth2.googleapis.com/token";
+    private static final String API_BASE_URL_INFO = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+    @Value("${google.client_id}")
+    private String clientId;
+
+    @Value("${google.client_secret}")
+    private String clientSecret;
+
+    @Value("${google.redirect_uri}")
+    private String redirectUri;
+
+    @Override
+    public TokenResponse getAccessToken(String authorizationCode) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", clientId);
+        body.add("client_secret", clientSecret);
+        body.add("redirect_uri", redirectUri);
+        body.add("code", authorizationCode);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            ResponseEntity<TokenResponse> response = restTemplate.postForEntity(
+                    API_BASE_URL_TOKEN,
+                    request,
+                    TokenResponse.class
+            );
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            log.error("구글 token API 요청 실패 : {}", e.getMessage());
+            throw new CustomException(ErrorCode.OAUTH_API_CALL_FAILED);
+        }
+    }
 
     @Override
     public OAuthUserInfo getUserInfo(String accessToken) {
         HttpHeaders headers = settingHeader(accessToken);
 
         try {
-            ResponseEntity<GoogleResponseDto> response = requestUserInfo(API_BASE_URL, headers, GoogleResponseDto.class);
+            ResponseEntity<GoogleResponseDto> response = requestUserInfo(API_BASE_URL_INFO, headers, GoogleResponseDto.class);
 
             return GoogleUserInfo.builder()
                     .response(response.getBody())
@@ -37,8 +83,4 @@ public class GoogleAuthService extends AbstractOAuthProviderService{
         }
     }
 
-    @Override
-    public TokenResponse getAccessToken(String authorizationCode) {
-        return null;
-    }
 }
