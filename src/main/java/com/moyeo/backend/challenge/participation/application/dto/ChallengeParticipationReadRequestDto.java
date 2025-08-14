6fa3fddb2c851faf46b6f0f8f3a01205b02ee88a
jelliclesu <jelliclesu@naver.com>
2025-08-14 15:46:59 +0900

    Refactor: [챌린지 관리] DateTimeFormat 적용 (MOYEO-60)
    - @JsonFormat 는 쿼리 파리미터에서 적용되지 않기 때문에, @DateTimeFormat 으로 변경

--- src/main/java/com/moyeo/backend/challenge/participation/application/dto/ChallengeParticipationReadRequestDto.java
+++ src/main/java/com/moyeo/backend/challenge/participation/application/dto/ChallengeParticipationReadRequestDto.java
@@ -1,12 +1,12 @@
 package com.moyeo.backend.challenge.participation.application.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.moyeo.backend.challenge.basic.domain.enums.ChallengeStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -18,7 +18,7 @@ import java.time.LocalDate;
 public class ChallengeParticipationReadRequestDto {
 
     @Schema(description = "날짜", example = "2025-08-14")
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate date;
 
     @Schema(description = "챌린지 상태", example = "INPROGRESS")
