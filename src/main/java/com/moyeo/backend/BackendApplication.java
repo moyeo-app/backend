96aa85abdbbc765d9eb757c30709a36e0fae7354
jelliclesu <jelliclesu@naver.com>
2025-08-26 16:07:03 +0900

    Chore: [사용자 관리] Spring Batch 설정 (MOYEO-73)

--- src/main/java/com/moyeo/backend/BackendApplication.java
+++ src/main/java/com/moyeo/backend/BackendApplication.java
@@ -3,9 +3,11 @@ package com.moyeo.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+@EnableScheduling
 public class BackendApplication {
 
 	public static void main(String[] args) {
